f3249e04608d94636435e00b2dc41e92a11d6a0b
Raúl Rodríguez <rrodriguez@ginzo.tech>
2022-07-10 19:19:19 +0200

    Moved main code into lib folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const colors = require('colors/safe')
-const { chromium } = require('playwright')
 const websites = require('./websites')
+const app = require('./lib/app')
 
 console.log(websites)
 console.log()
@@ -16,37 +16,6 @@ const stores = websites.reduce((storeNames, website) => {
 console.log(`${colors.blue('\t*')} ${stores.join(colors.blue('\n\t* '))}`)
 console.log()
 
-setInterval(async () => {
-  const browser = await chromium.launch()
+app(websites)
 
-  for (const website of websites) {
-    console.log(`Opening page: ${website.name}`)
-    const context = await browser.newContext()
-    try {
-      const page = await context.newPage()
-      await page.goto(website.url)
-      const selector = await page.textContent(website.selector, {
-        timeout: 5000,
-      })
-      if (selector.includes(website.content)) {
-        console.log(
-          `${website.name} HAS ${website.type} ${colors.brightGreen(
-            'IN STOCK'
-          )}`
-        )
-        console.log(colors.underline(website.url))
-      } else {
-        throw new Error(
-          `${website.name} DOES NOT HAVE ${website.type} IN STOCK`
-        )
-      }
-    } catch (_) {
-      console.log(`${website.type} ${colors.red('OUT OF STOCK')}`)
-    } finally {
-      await context.close()
-      console.log()
-    }
-  }
-
-  await browser.close()
-}, 1000 * 60 * 10) // every 10 minutes
+setInterval(async () => app(websites), 1000 * 60 * 10) // every 10 minutes
